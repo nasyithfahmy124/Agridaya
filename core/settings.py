87950da1c6c4e078428223e541917c82b8ba93c3
Nasyith Fahmy <nasyithfahmy124@gmail.com>
2026-07-16 02:49:37 +0700

    hostinh

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
-
-
+ALLOWED_HOSTS = [
+    'agridaya-kei4.vercel.app', 
+    'localhost', 
+    '127.0.0.1'
+]
 # Application definition
 
 INSTALLED_APPS = [
